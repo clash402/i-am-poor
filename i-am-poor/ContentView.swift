f76a54b925524ecb425d0612de1dc3c155425a74
Josh Courtney <joshrcourtney@gmail.com>
2021-05-16 15:47:42 -0500

    Update project

--- i-am-poor/ContentView.swift
+++ i-am-poor/ContentView.swift
@@ -9,8 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Rectangle()
+                .edgesIgnoringSafeArea(.all)
+                .foregroundColor(Color.init(red: 0/255, green: 172/255, blue: 117/255))
+            VStack {
+                Text("I Am Poor")
+                    .font(Font.custom("Helvetica Neue", size: 56))
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                    .padding(.all, 2)
+                Text("So don't ask for anything! 😠")
+                    .font(Font.custom("Helvetica Neue", size: 24))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                Image("coal")
+                    .aspectRatio(1, contentMode: .fill)
+                    .padding(.vertical, 80)
+            }
+        }
     }
 }
 
